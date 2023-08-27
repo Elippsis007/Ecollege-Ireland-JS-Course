871460fcc25cd0c01cad125ecca90295209888cb
Elippsis007 <michaeldrmm@gmail.com>
2023-08-27 12:19:42 +0100

    git pull origin master

---
+++ functions.js
@@ -0,0 +1,7 @@
+function greetUser() {
+    // Where the output will be on the "result" div
+    document.getElementById('result').innerHTML = "Hello User!";
+}
+function sayBye() {
+    document.getElementById("result").innerHTML = "Buh Bye!";
+}
